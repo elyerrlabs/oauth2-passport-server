7c7c91465289a0ffa259cc53ca4fa1a46ef676e1
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-08-21 13:03:23 -0500

    feat: add support to enable user age registration on settings
    Signed-off-by: Elvis Yerel Roman <yerel9212@yahoo.es>

--- app/Services/Settings/Setting.php
+++ app/Services/Settings/Setting.php
@@ -23,7 +23,7 @@ namespace App\Services\Settings;
  * 
  * SPDX-License-Identifier: LicenseRef-NC-Open-Source-Project
  */
- 
+
 use App\Models\OAuth\Token;
 use Illuminate\Support\Str;
 use App\Models\OAuth\Client;
@@ -36,9 +36,9 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Database\QueryException;
 
-class Setting  
+class Setting
 {
-    
+
     /**
      * Set default values 
      * @return void
@@ -58,7 +58,7 @@ class Setting
         Setting::getRoutesSettings();
         Setting::getRateLimitSettings();
 
-        if (config('system.schema_mode', 'https') == 'https') { 
+        if (config('system.schema_mode', 'https') == 'https') {
             URL::forceScheme('https');
         }
 
@@ -280,6 +280,8 @@ class Setting
         settingLoad('system.privacy_url', null);
         settingLoad('system.terms_url', null);
         settingLoad('system.policy_cookies', null);
+        settingLoad('system.birthday.active', false);
+        settingLoad('system.birthday.limit', 18);
 
         //Session settings
         //settingLoad('session.driver', 'database');
@@ -554,7 +556,7 @@ class Setting
      * @return void
      */
     public static function getSystemSetting()
-    { 
+    {
         Config::set('system.home_page', settingItem('system.home_page', "/"));
         Config::set('system.cookie_name', settingItem('system.cookie_name', null));
         Config::set('system.passport_token_services', settingItem('system.passport_token_services', null));
@@ -567,6 +569,8 @@ class Setting
         Config::set('system.privacy_url', settingItem('system.privacy_url', null));
         Config::set('system.terms_url', settingItem('system.terms_url', null));
         Config::set('system.policy_cookies', settingItem('system.policy_cookies', null));
+        Config::set('system.birthday.active', settingItem('system.birthday.active', false));
+        Config::set('system.birthday.limit', settingItem('system.birthday.limit', 18));
 
     }
 
