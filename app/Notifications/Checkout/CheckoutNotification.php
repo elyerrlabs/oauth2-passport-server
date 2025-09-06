6343e01f663fb96252ff7b0e41ad81482ca82c5d
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-09-05 22:22:21 -0500

    feat: add checkout notification
    Signed-off-by: Elvis Yerel Roman <yerel9212@yahoo.es>

---
+++ app/Notifications/Checkout/CheckoutNotification.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Notifications\Checkout;
+
+/**
+ * Copyright (c) 2025 Elvis Yerel Roman Concha
+ *
+ * This file is part of an open source project licensed under the
+ * "NON-COMMERCIAL USE LICENSE - OPEN SOURCE PROJECT" (Effective Date: 2025-08-03).
+ *
+ * You may use, study, modify, and redistribute this file for personal,
+ * educational, or non-commercial research purposes only.
+ *
+ * Commercial use is strictly prohibited without prior written consent
+ * from the author.
+ *
+ * Combining this software with any project licensed for commercial use
+ * (such as AGPL) is not permitted without explicit authorization.
+ *
+ * This software supports OAuth 2.0 and OpenID Connect.
+ *
+ * Author Contact: yerel9212@yahoo.es
+ *
+ * SPDX-License-Identifier: LicenseRef-NC-Open-Source-Project
+ */
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CheckoutNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $url;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(string $url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject(__('Purchase Confirmation'))
+            ->greeting(__('Hello :name', ['name' => $notifiable->name]))
+            ->line(__('We have successfully received your order.'))
+            ->line(__('Your purchase has been confirmed and is being processed.'))
+            ->action(__('View your order'), $this->url)
+            ->line(__('Thank you for shopping with us!'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'title' => __('Order Successfully Received'),
+            'message' => __('Your order was received and is being processed.'),
+            'url' => $this->url,
+        ];
+    }
+
+}
