81821c4e10cf51a2b2c3203f3344b236c311071b
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-11-14 11:52:17 -0500

    fix: remove partner.generate route
    Signed-off-by: Elvis Yerel Roman <yerel9212@yahoo.es>

--- core/Partner/routes/web.php
+++ core/Partner/routes/web.php
@@ -29,6 +29,4 @@ Route::middleware(['throttle:partner:web'])->group(function () {
     Route::get('dashboard', [PartnerController::class, 'dashboard'])->name('dashboard');
     Route::get('sales', [PartnerController::class, 'sales'])->name('sales');
     Route::get('generate', [PartnerController::class, 'show'])->name('show');
-    Route::post('generate', [PartnerController::class, 'generate'])->name('generate');
-
 });
