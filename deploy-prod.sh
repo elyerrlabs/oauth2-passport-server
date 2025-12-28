42bbea27d4aa1b1ddde88ed21cf983c573dd43ad
Elvis Yerel Roman <yerel9212@yahoo.es>
2025-12-28 13:07:34 -0500

    chore: set new version to v5.0.2
    Signed-off-by: Elvis Yerel Roman <yerel9212@yahoo.es>

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -22,7 +22,7 @@
 # SPDX-License-Identifier: LicenseRef-NC-Open-Source-Project
 # -----------------------------------------------------------------------------
 
-IMAGE="elyerr/oauth2-passport-server:v5.0.1"
+IMAGE="elyerr/oauth2-passport-server:v5.0.2"
 COMPOSE_FILE="docker-compose-prod.yml"
 ENV_FILE=".env"
 
