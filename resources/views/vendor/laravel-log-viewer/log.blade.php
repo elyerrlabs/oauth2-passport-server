c535b100a94c66821be49becb8d7ef6d8b8b852f
Elvis Yerel Roman <yerel9212@yahoo.es>
2026-02-27 18:28:10 -0500

    fix: remove orphan route
    Signed-off-by: Elvis Yerel Roman <yerel9212@yahoo.es>

--- resources/views/vendor/laravel-log-viewer/log.blade.php
+++ resources/views/vendor/laravel-log-viewer/log.blade.php
@@ -184,14 +184,6 @@
     <div class="container-fluid">
         <div class="row">
             <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4 col-12">
-                <a class="navbar-brand" href="{{ route('user.admin.dashboard') }}">
-                    {{ __('Admin Dashboard') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
