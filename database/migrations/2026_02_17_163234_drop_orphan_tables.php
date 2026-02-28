e34bff8e9de35546f73ccf78d32eafb240f9b0ff
Elvis Yerel Roman <yerel9212@yahoo.es>
2026-02-28 11:19:58 -0500

    fix(migrations): updated orphan tables (delete checkouts)
    Signed-off-by: Elvis Yerel Roman <yerel9212@yahoo.es>

--- database/migrations/2026_02_17_163234_drop_orphan_tables.php
+++ database/migrations/2026_02_17_163234_drop_orphan_tables.php
@@ -29,8 +29,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -42,6 +41,7 @@ return new class extends Migration
             'variants',
             'orders',
             'attributes',
+            'checkouts',
             'tags',
             'categories',
             'units',
