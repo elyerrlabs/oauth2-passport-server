cd37c2540f93d9e68dc8c808951699c6d213f7a1
Elvis Yerel Roman <yerel9212@yahoo.es>
2026-05-24 22:49:55 -0500

    feat: add new command to delete config keys

---
+++ app/Console/Commands/deleteConfigKey.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+/**
+ * OAuth2 Passport Server — a centralized, modular authorization server
+ * implementing OAuth 2.0 and OpenID Connect specifications.
+ *
+ * Copyright (c) 2026 Elvis Yerel Roman Concha
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Author: Elvis Yerel Roman Concha
+ * Contact: yerel9212@yahoo.es
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+use App\Services\SettingService;
+use Illuminate\Console\Command;
+
+class deleteConfigKey extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = "config:delete-key {prefix}";
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete config keys using prefix name like \'routes.\'';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $prefix = $this->argument('prefix');
+
+        app(SettingService::class)->deleteKeysByModule($prefix);
+        app(SettingService::class)->resetConfigKeys();
+
+        $this->info("Config keys has been deleted.");
+    }
+}
