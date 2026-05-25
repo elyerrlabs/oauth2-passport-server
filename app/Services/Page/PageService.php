9e7e3e86369ef21cc3055b7edad2e50bfcc59f75
Elvis Yerel Roman <yerel9212@yahoo.es>
2026-05-24 22:18:17 -0500

    fix: updated favicon default layout

--- app/Services/Page/PageService.php
+++ app/Services/Page/PageService.php
@@ -295,10 +295,11 @@ final class PageService extends \App\Services\Page\Service
                     $templateFavicon = <<<BLADE
                     <!-- Favicon & Browser Icons -->
                     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+                    <!--
                     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
                     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
-                    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
-                    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+                    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') 
+                    -->
                     <meta name="theme-color" content="#ffffff">
                     <meta name="msapplication-TileColor" content="#ffffff">
                     BLADE;
--- resources/views/pages/layouts/favicon.blade.php
+++ resources/views/pages/layouts/favicon.blade.php
@@ -1,1 +1,9 @@
-<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<!-- Favicon & Browser Icons -->
+<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+<!--
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') 
+-->
+<meta name="theme-color" content="#ffffff">
+<meta name="msapplication-TileColor" content="#ffffff">
