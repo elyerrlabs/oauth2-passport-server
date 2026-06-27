6c56371fe34005278cade31dd7b26fcb2c796f99
Elvis Yerel Roman <yerel9212@yahoo.es>
2026-06-26 22:54:56 -0500

    chore: update log.blade.php
    Signed-off-by: Elvis Yerel Roman <yerel9212@yahoo.es>

--- resources/views/vendor/laravel-log-viewer/log.blade.php
+++ resources/views/vendor/laravel-log-viewer/log.blade.php
@@ -1,16 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="noindex, nofollow">
-    <title>{{ __('Laravel log viewer') }}</title>
-    <link nonce="{{ $nonce }}" rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link nonce="{{ $nonce }}" rel="stylesheet" type="text/css"
-        href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
+    <title>{{ config('app.name', 'Laravel') }} - {{ __('Logs') }}</title>
+    <link nonce="{{ $nonce }}" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link nonce="{{ $nonce }}" rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
     <style nonce="{{ $nonce }}">
         body {
             padding: 25px;
@@ -73,14 +69,8 @@
             padding: 5px;
         }
 
-        .stack {
-            font-size: 0.85em;
-            white-space: pre-wrap;
-        }
 
-        .stack.hidden {
-            display: none;
-        }
+
 
         /**
     * DARK MODE CSS
@@ -118,7 +108,8 @@
         }
 
         [data-theme="dark"] .table td,
-        [data-theme="dark"] .table th,[data-theme="dark"] .table thead th {
+        [data-theme="dark"] .table th,
+        [data-theme="dark"] .table thead th {
             border-color: #616161;
         }
 
@@ -156,6 +147,7 @@
             background-color: #212121;
             border-color: #4a4a4a;
         }
+
     </style>
 
     <script nonce="{{ $nonce }}">
@@ -177,198 +169,173 @@
                 localStorage.removeItem('darkSwitch');
             }
         }
+
     </script>
 </head>
-
 <body>
     <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light mb-2 col-12">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link font-bold" href="{{ route('user.dashboard') }}">{{ __('Dashboard') }}</a>
+                    </li>
+                    <li class="nav-item font-bold">
+                        <a class="nav-link" href="{{ route('admin.settings.general') }}">{{ __('Settings') }}</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
         <div class="row">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4 col-12">
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('user.dashboard') }}">{{ __('User Dashboard') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('admin.settings.general') }}">{{ __('Settings') }}</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-
-
             <div class="col sidebar mb-3">
-                <h1><i class="fa fa-calendar" aria-hidden="true"></i> {{ __('Laravel Log Viewer') }}</h1>
-                <p class="text-muted"><i>by Rap2h</i></p>
+                <h1><i class="fa fa-calendar" aria-hidden="true"></i>Logs</h1>
 
-                <div class="custom-control custom-switch">
+                <div class="custom-control custom-switch" style="padding-bottom:20px;">
                     <input type="checkbox" class="custom-control-input" id="darkSwitch">
-                    <label class="custom-control-label" for="darkSwitch">{{ __('Dark Mode') }}</label>
+                    <label class="custom-control-label" for="darkSwitch" style="margin-top: 6px;">Dark Mode</label>
                 </div>
 
                 <div class="list-group div-scroll">
-                    @foreach ($folders as $folder)
-                        <div class="list-group-item">
-                            <?php
-                            \Rap2hpoutre\LaravelLogViewer\LaravelLogViewer::DirectoryTreeStructure($storage_path, $structure);
-                            ?>
+                    @foreach($folders as $folder)
+                    <div class="list-group-item">
+                        <?php
+            \Rap2hpoutre\LaravelLogViewer\LaravelLogViewer::DirectoryTreeStructure( $storage_path, $structure );
+            ?>
 
-                        </div>
+                    </div>
                     @endforeach
-                    @foreach ($files as $file)
-                        <a href="?l={{ \Illuminate\Support\Facades\Crypt::encrypt($file) }}"
-                            class="list-group-item @if ($current_file == $file) llv-active @endif">
-                            {{ $file }}
-                        </a>
+                    @foreach($files as $file)
+                    <a href="?l={{ \Illuminate\Support\Facades\Crypt::encryptString($file) }}" class="list-group-item @if ($current_file == $file) llv-active @endif">
+                        {{$file}}
+                    </a>
                     @endforeach
                 </div>
             </div>
             <div class="col-10 table-container">
                 @if ($logs === null)
-                    <div>
-                        {{ __('Log file >50M, please download it.') }}
-                    </div>
+                <div>
+                    Log file >50M, please download it.
+                </div>
                 @else
-                    <table id="table-log" class="table table-striped"
-                        data-ordering-index="{{ $standardFormat ? 2 : 0 }}">
-                        <thead>
-                            <tr>
-                                @if ($standardFormat)
-                                    <th>{{ __('Level') }}</th>
-                                    <th>{{ __('Context') }}</th>
-                                    <th>{{ __('Date') }}</th>
-                                @else
-                                    <th>{{ __('Line number') }}</th>
+                <table id="table-log" class="table table-striped" data-ordering-index="{{ $standardFormat ? 2 : 0 }}">
+                    <thead>
+                        <tr>
+                            @if ($standardFormat)
+                            <th>Level</th>
+                            <th>Context</th>
+                            <th>Date</th>
+                            @else
+                            <th>Line number</th>
+                            @endif
+                            <th>Content</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        @foreach($logs as $key => $log)
+                        <tr data-display="stack{{{$key}}}">
+                            @if ($standardFormat)
+                            <td class="nowrap text-{{{$log['level_class']}}}">
+                                <span class="fa fa-{{{$log['level_img']}}}" aria-hidden="true"></span>&nbsp;&nbsp;{{$log['level']}}
+                            </td>
+                            <td class="text">{{$log['context']}}</td>
+                            @endif
+                            <td class="date">{{{$log['date']}}}</td>
+                            <td class="text">
+                                @if ($log['stack'])
+                                <button type="button" class="float-right expand btn btn-outline-dark btn-sm mb-2 ml-2" data-display="stack{{{$key}}}">
+                                    <span class="fa fa-search"></span>
+                                </button>
                                 @endif
-                                <th>{{ __('Content') }}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+                                {{{$log['text']}}}
+                                @if (isset($log['in_file']))
+                                <br />{{{$log['in_file']}}}
+                                @endif
+                                @if ($log['stack'])
+                                <div class="stack" id="stack{{{$key}}}" style="display: none; white-space: pre-wrap;">{{{ trim($log['stack']) }}}
+                                </div>
+                                @endif
+                            </td>
+                        </tr>
+                        @endforeach
 
-                            @foreach ($logs as $key => $log)
-                                <tr data-display="stack{{ $key }}">
-                                    @if ($standardFormat)
-                                        <td class="nowrap text-{{ $log['level_class'] }}">
-                                            <span class="fa fa-{{ $log['level_img'] }}"
-                                                aria-hidden="true"></span>&nbsp;&nbsp;{{ $log['level'] }}
-                                        </td>
-                                        <td class="text">{{ $log['context'] }}</td>
-                                    @endif
-                                    <td class="date">{{ $log['date'] }}</td>
-                                    <td class="text">
-                                        @if (auth()->user()->hasAccess(['administrator:logs:full']) && auth()->user()->email != config('system.demo.email'))
-                                            @if ($log['stack'])
-                                                <button type="button"
-                                                    class="float-right expand btn btn-outline-dark btn-sm mb-2 ml-2"
-                                                    data-display="stack{{ $key }}">
-                                                    <span class="fa fa-search"></span>
-                                                </button>
-                                            @endif
-                                        @endif
-                                        {{ $log['text'] }}
-                                        @if (isset($log['in_file']))
-                                            <br />{{ $log['in_file'] }}
-                                        @endif
-                                        @if ($log['stack'])
-                                            <div class="stack hidden" id="stack{{ $key }}">
-                                                {{ trim($log['stack']) }}
-                                            </div>
-                                        @endif
-
-                                    </td>
-                                </tr>
-                            @endforeach
-
-                        </tbody>
-                    </table>
+                    </tbody>
+                </table>
                 @endif
                 <div class="p-3">
-                    @if (auth()->user()->hasAccess(['administrator:logs:full']) && auth()->user()->email != config('system.demo.email'))
-                        @if ($current_file)
-                            <a
-                                href="?dl={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ $current_folder ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
-                                <span class="fa fa-download"></span> {{ __('Download file') }}
-                            </a>
-                            -
-                            <a id="clean-log"
-                                href="?clean={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ $current_folder ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
-                                <span class="fa fa-sync"></span> {{ __('Clean file') }}
-                            </a>
-                            -
-                            <a id="delete-log"
-                                href="?del={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ $current_folder ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
-                                <span class="fa fa-trash"></span> {{ __('Delete file') }}
-                            </a>
-                            @if (count($files) > 1)
-                                -
-                                <a id="delete-all-log"
-                                    href="?delall=true{{ $current_folder ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
-                                    <span class="fa fa-trash-alt"></span> {{ __('Delete all files') }}
-                                </a>
-                            @endif
-                        @endif
+                    @if($current_file)
+                    <a href="?dl={{ \Illuminate\Support\Facades\Crypt::encryptString($current_file) }}{{ ($current_folder) ? '&f=' . \Illuminate\Support\Facades\Crypt::encryptString($current_folder) : '' }}">
+                        <span class="fa fa-download"></span> Download file
+                    </a>
+                    -
+                    <a id="clean-log" href="?clean={{ \Illuminate\Support\Facades\Crypt::encryptString($current_file) }}{{ ($current_folder) ? '&f=' . \Illuminate\Support\Facades\Crypt::encryptString($current_folder) : '' }}">
+                        <span class="fa fa-sync"></span> Clean file
+                    </a>
+                    -
+                    <a id="delete-log" href="?del={{ \Illuminate\Support\Facades\Crypt::encryptString($current_file) }}{{ ($current_folder) ? '&f=' . \Illuminate\Support\Facades\Crypt::encryptString($current_folder) : '' }}">
+                        <span class="fa fa-trash"></span> Delete file
+                    </a>
+                    @if(count($files) > 1)
+                    -
+                    <a id="delete-all-log" href="?delall=true{{ ($current_folder) ? '&f=' . \Illuminate\Support\Facades\Crypt::encryptString($current_folder) : '' }}">
+                        <span class="fa fa-trash-alt"></span> Delete all files
+                    </a>
+                    @endif
+                    @endif
                 </div>
-                @endif
             </div>
         </div>
     </div>
     <!-- jQuery for Bootstrap -->
-    <script nonce="{{ $nonce }}" src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script nonce="{{ $nonce }}" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
+    <script nonce="{{ $nonce }}" src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script nonce="{{ $nonce }}" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <!-- FontAwesome -->
     <script nonce="{{ $nonce }}" defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
     <!-- Datatables -->
-    <script type="text/javascript" nonce="{{ $nonce }}"
-        src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" nonce="{{ $nonce }}"
-        src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+    <script nonce="{{ $nonce }}" type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script nonce="{{ $nonce }}" type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
 
     <script nonce="{{ $nonce }}">
-        document.addEventListener('DOMContentLoaded', function() {
-            // dark mode by https://github.com/coliff/dark-mode-switch
-            const darkSwitch = document.getElementById('darkSwitch');
+        // dark mode by https://github.com/coliff/dark-mode-switch
+        const darkSwitch = document.getElementById('darkSwitch');
 
-            // this is here so we can get the body dark mode before the page displays
-            // otherwise the page will be white for a second... 
-            initTheme();
+        // this is here so we can get the body dark mode before the page displays
+        // otherwise the page will be white for a second... 
+        initTheme();
 
-            window.addEventListener('load', () => {
-                if (darkSwitch) {
-                    initTheme();
-                    darkSwitch.addEventListener('change', () => {
-                        resetTheme();
-                    });
+        window.addEventListener('load', () => {
+            if (darkSwitch) {
+                initTheme();
+                darkSwitch.addEventListener('change', () => {
+                    resetTheme();
+                });
+            }
+        });
+
+        // end darkmode js
+
+        $(document).ready(function() {
+            $('.table-container tr').on('click', function() {
+                $('#' + $(this).data('display')).toggle();
+            });
+            $('#table-log').DataTable({
+                "order": [$('#table-log').data('orderingIndex'), 'desc']
+                , "stateSave": true
+                , "stateSaveCallback": function(settings, data) {
+                    window.localStorage.setItem("datatable", JSON.stringify(data));
+                }
+                , "stateLoadCallback": function(settings) {
+                    var data = JSON.parse(window.localStorage.getItem("datatable"));
+                    if (data) data.start = 0;
+                    return data;
                 }
             });
-
-            // end darkmode js 
-            @if (auth()->user()->hasAccess(['administrator:logs:full']) && auth()->user()->email != config('system.demo.email'))
-
-                $('.table-container tr').on('click', function() {
-                    $('#' + $(this).data('display')).toggle();
-                });
-                $('#table-log').DataTable({
-                    "order": [$('#table-log').data('orderingIndex'), 'desc'],
-                    "stateSave": true,
-                    "stateSaveCallback": function(settings, data) {
-                        window.localStorage.setItem("datatable", JSON.stringify(data));
-                    },
-                    "stateLoadCallback": function(settings) {
-                        var data = JSON.parse(window.localStorage.getItem("datatable"));
-                        if (data) data.start = 0;
-                        return data;
-                    }
-                });
-                $('#delete-log, #clean-log, #delete-all-log').click(function() {
-                    return confirm('Are you sure?');
-                });
-            @endif
+            $('#delete-log, #clean-log, #delete-all-log').click(function() {
+                return confirm('Are you sure?');
+            });
         });
+
     </script>
 </body>
-
 </html>
